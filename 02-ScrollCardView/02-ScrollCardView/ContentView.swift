8a6db8205ede1d486f13ba40511e3ad97c8b29ad
EstrHuP <esthrhup@gmail.com>
2021-10-14 23:44:21 +0200

    New section of course - Scroll and CardView

---
+++ 02-ScrollCardView/02-ScrollCardView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  02-ScrollCardView
+//
+//  Created by Esther Huecas on 14/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 02-ScrollCardView/02-ScrollCardView/_2_ScrollCardViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  _2_ScrollCardViewApp.swift
+//  02-ScrollCardView
+//
+//  Created by Esther Huecas on 14/10/21.
+//
+
+import SwiftUI
+
+@main
+struct _2_ScrollCardViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
