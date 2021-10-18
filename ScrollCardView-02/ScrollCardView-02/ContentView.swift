11a00f01378b562d27ede915a98b629469f7f36c
EstrHuP <esthrhup@gmail.com>
2021-10-18 22:08:21 +0200

    Updated: Design screen with two carrousel

--- ScrollCardView-02/ScrollCardView-02/ContentView.swift
+++ ScrollCardView-02/ScrollCardView-02/ContentView.swift
@@ -10,9 +10,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
     
-        ScrollView {
+        VStack {
             HeaderView()
-            ContentScrollView()
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    FirstCarrouselView()
+                        .frame(width: 300)
+                }
+            }
+            Spacer()
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    SecondCarrouselView()
+                        .frame(width: 300)
+                }
+            }
+            Spacer()
         }
     }
 }
@@ -43,12 +58,12 @@ struct HeaderView: View {
                 .font(.system(.footnote, design: .rounded))
                 .fontWeight(.black)
                 .foregroundColor(.secondary)
-            
         }
+        .padding([.bottom, .horizontal])
     }
 }
 
-struct ContentScrollView: View {
+struct FirstCarrouselView: View {
     var body: some View {
         
         CardView(image: "swift", author: "Juan Gabriel Gomila", title: "diseño de apps para ios13 con swiftui desde cero", originalPrice: "199.99$", discountPrice: "10.99$")
@@ -58,6 +73,20 @@ struct ContentScrollView: View {
         CardView(image: "maths", author: "Ricardo Alberich, Juan Gabriel Gomila y Arnaur Mir", title: "curso de probabilidad y variable aleatorias para machine learning", originalPrice: "199.99$", discountPrice: "12.99$")
         
         CardView(image: "python", author: "Juan Gabriel Gomila y Llorenç Valverde", title: "resuelve problemas de matemáticas con sage y python", originalPrice: "99.99$", discountPrice: "10.99$")
+        
+    }
+}
+
+struct SecondCarrouselView: View {
+    var body: some View {
+        
+        CardView(image: "maths", author: "Ricardo Alberich, Juan Gabriel Gomila y Arnaur Mir", title: "curso de probabilidad y variable aleatorias para machine learning", originalPrice: "199.99$", discountPrice: "12.99$")
+        
+        CardView(image: "python", author: "Juan Gabriel Gomila y Llorenç Valverde", title: "resuelve problemas de matemáticas con sage y python", originalPrice: "199.99$", discountPrice: "10.99$")
+        
+        CardView(image: "swift", author: "Juan Gabriel Gomila", title: "diseño de apps para ios13 con swiftui desde cero", originalPrice: "199.99$", discountPrice: "10.99$")
+        
+        CardView(image: "videogames", author: "Juan Gabriel Gomila", title: "curso completo de videojuegos con unity 2019", originalPrice: "199.99$", discountPrice: "11.99$")
         
     }
 }
