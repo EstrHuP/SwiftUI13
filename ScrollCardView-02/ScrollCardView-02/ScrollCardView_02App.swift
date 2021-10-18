302ef5f1d1d8b876de43a0618c5fb4296903eb1e
EstrHuP <esthrhup@gmail.com>
2021-10-18 18:36:52 +0200

    Remove 02-ScrollCardView project because simulator preview doesn't work. Create this project but with new name

---
+++ ScrollCardView-02/ScrollCardView-02/ScrollCardView_02App.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollCardView_02App.swift
+//  ScrollCardView-02
+//
+//  Created by Esther Huecas on 18/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrollCardView_02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
