f774107644ac46103bf0e6543a01d818776598a9
EstrHuP <esthrhup@gmail.com>
2021-10-19 18:08:47 +0200

    New Section 03 - Buttons

---
+++ Buttons-03/Buttons-03/Buttons_03App.swift
@@ -0,0 +1,17 @@
+//
+//  Buttons_03App.swift
+//  Buttons-03
+//
+//  Created by Esther Huecas on 19/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Buttons_03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Buttons-03/Buttons-03/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Buttons-03
+//
+//  Created by Esther Huecas on 19/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
