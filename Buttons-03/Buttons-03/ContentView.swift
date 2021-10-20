30ac8f01fc4495be459d342741de33b5f05e44e7
EstrHuP <esthrhup@gmail.com>
2021-10-20 12:18:35 +0200

    New: init new section 03 - Buttons

--- Buttons-03/Buttons-03/ContentView.swift
+++ Buttons-03/Buttons-03/ContentView.swift
@@ -9,8 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button(action: {
+                print("Button pressed")
+            }) {
+                Text("I'm a button")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(50)
+                
+                    .padding(12)
+                    .overlay (
+                    RoundedRectangle(cornerRadius: 50)
+                        .stroke(Color.black, lineWidth: 5)
+                    )
+            }
+            
+            Button(action: {
+                print("Button with icon pressed")
+            }) {
+                HStack {
+                    Image(systemName: "trash")
+                    Text("Delete")
+                        .fontWeight(.bold)
+                }
+                .padding()
+                /// UIGradients --> web for gradients
+                .background(LinearGradient(gradient: Gradient(colors: [Color("Dark Ocean 1"), Color("Dark Ocean 2")]), startPoint: .leading, endPoint: .trailing))
+//                .clipShape(Circle())
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .cornerRadius(40)
+//                .shadow(radius: 10.0) //Sombreado
+                .shadow(color: .blue, radius: 10.0, x: 20, y: 5)
+            }
+        }
     }
 }
 
