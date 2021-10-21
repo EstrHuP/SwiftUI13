f8617123b71b5542d90cf74300ef8165f2f9acb0
EstrHuP <esthrhup@gmail.com>
2021-10-21 10:41:21 +0200

    New: init new Section 04 - States & Bindings

---
+++ States_Bindings-04/States_Bindings-04/States_Bindings_04App.swift
@@ -0,0 +1,17 @@
+//
+//  States_Bindings_04App.swift
+//  States_Bindings-04
+//
+//  Created by Esther Huecas on 21/10/21.
+//
+
+import SwiftUI
+
+@main
+struct States_Bindings_04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
