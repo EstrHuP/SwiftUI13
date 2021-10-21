70b3def2426546ebda61931cc7243c54dd4d9614
EstrHuP <esthrhup@gmail.com>
2021-10-21 14:20:16 +0200

    Updated Exercise 1 with some struct for clean code

--- States_Bindings-04/States_Bindings-04/Exercise_1.swift
+++ States_Bindings-04/States_Bindings-04/Exercise_1.swift
@@ -9,52 +9,30 @@ import SwiftUI
 
 struct Exercise_1: View {
     
-    @State private var count = 0
-    @State private var result = 0
+    @State private var numClicks = 0
+    @State private var total = 0
     
     var body: some View {
         
         VStack {
             Text("Botón contador")
             
-            //Count's Button
-            Button {
-                count += 1
-            } label: {
-                Circle()
-                    .frame(width: 150, height: 150)
-                    .foregroundColor(.blue)
-                    .overlay(Text("\(count)"))
-                    .font(.system(size: 50, weight: .black, design: .rounded))
-                    .foregroundColor(.white)
-            }
+            CounterView(numClicks: $numClicks, buttonColor: .blue)
             
             //Minus or plus result with two buttons
             Text("Botón añadir y quitar")
+            
                 .padding(.top, 100)
             Circle()
                 .frame(width: 150, height: 150)
                 .foregroundColor(.purple)
-                .overlay(Text("\(result)"))
+                .overlay(Text("\(total)"))
                 .font(.system(size: 50, weight: .black, design: .rounded))
                 .foregroundColor(.white)
             
             HStack {
-                Button {
-                    self.result -= 1
-                } label: {
-                    Image(systemName: "minus.circle.fill")
-                        .font(.system(size: 100))
-                        .foregroundColor(.red)
-                }
-                
-                Button {
-                    self.result += 1
-                } label: {
-                    Image(systemName: "plus.circle.fill")
-                        .font(.system(size: 100))
-                        .foregroundColor(.green)
-                }
+                MinusView(total: $total)
+                PlusView(total: $total)
             }
         } .padding()
     }
@@ -63,5 +41,56 @@ struct Exercise_1: View {
 struct Exercise_1_Previews: PreviewProvider {
     static var previews: some View {
         Exercise_1()
+    }
+}
+
+struct CounterView: View {
+    
+    @Binding var numClicks: Int
+    var buttonColor: Color
+    
+    var body: some View {
+        
+        //Count's Button
+        Button {
+            numClicks += 1
+        } label: {
+            Circle()
+                .frame(width: 150, height: 150)
+                .foregroundColor(buttonColor)
+                .overlay(Text("\(numClicks)"))
+                .font(.system(size: 50, weight: .black, design: .rounded))
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct MinusView: View {
+    
+    @Binding var total: Int
+    
+    var body: some View {
+        Button {
+            self.total -= 1
+        } label: {
+            Image(systemName: "minus.circle.fill")
+                .font(.system(size: 100))
+                .foregroundColor(.red)
+        }
+    }
+}
+
+struct PlusView: View {
+    
+    @Binding var total: Int
+    
+    var body: some View {
+        Button {
+            self.total += 1
+        } label: {
+            Image(systemName: "plus.circle.fill")
+                .font(.system(size: 100))
+                .foregroundColor(.green)
+        }
     }
 }
