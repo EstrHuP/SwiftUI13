ad7d0ddb414c7fdddb842105d05ed35ee029bc2a
EstrHuP <esthrhup@gmail.com>
2021-10-26 19:46:41 +0200

    New: init new Section 06 - Animations || Animaciones implicitas

---
+++ Animations-06/Animations-06/Animations_06App.swift
@@ -0,0 +1,17 @@
+//
+//  Animations_06App.swift
+//  Animations-06
+//
+//  Created by Esther Huecas on 26/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Animations_06App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
