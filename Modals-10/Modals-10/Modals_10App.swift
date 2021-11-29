1629394ca46f1202f3598f1ea014913049b6aa8f
EstrHuP <esthrhup@gmail.com>
2021-11-29 15:57:42 +0100

    New: init section 10-Modals. Create modals with sheet

---
+++ Modals-10/Modals-10/Modals_10App.swift
@@ -0,0 +1,17 @@
+//
+//  Modals_10App.swift
+//  Modals-10
+//
+//  Created by Esther Huecas on 29/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Modals_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Modals-10/Modals-10/VideogamesDetailView.swift
@@ -0,0 +1,46 @@
+//
+//  VideogamesDetailView.swift
+//  Modals-10
+//
+//  Created by Esther Huecas on 29/11/21.
+//
+
+import SwiftUI
+
+struct VideogameDetailView: View {
+    @Environment(\.presentationMode) var presentationMode
+    var videogame: Videogame
+    
+    var body: some View {
+        VStack {
+            Image(videogame.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Text(videogame.name)
+                .font(.system(.title, design: .rounded).bold())
+                .multilineTextAlignment(.center)
+                .frame(width: 300)
+                .lineLimit(2)
+            
+            Spacer()
+        }
+        //        .navigationBarTitle("", displayMode: .inline)
+        .edgesIgnoringSafeArea(.top)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(
+            leading: Button(action: {
+            //Navegar a la pantalla previa
+                self.presentationMode.wrappedValue.dismiss()
+        }, label: {
+            Image(systemName: "arrow.left.circle.fill")
+                .font(.title)
+                .foregroundColor(.white)
+        }))
+    }
+}
+
+struct VideogameDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideogameDetailView(videogame: Videogame(name: "Dead Cells", image: "dead-cells"))
+    }
+}
