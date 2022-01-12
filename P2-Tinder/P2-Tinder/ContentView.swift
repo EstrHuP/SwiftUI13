1986044d09a174f9772ee1da6410c0a5b61e4e5a
EstrHuP <esthrhup@gmail.com>
2022-01-12 19:26:20 +0100

    New: content design and show structs in Preview with differents modes

--- P2-Tinder/P2-Tinder/ContentView.swift
+++ P2-Tinder/P2-Tinder/ContentView.swift
@@ -9,13 +9,74 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            TopBarView()
+            CardView(course: coursesArray[0])
+            BottomBarView()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            TopBarView().previewLayout(.sizeThatFits)
+            BottomBarView().previewLayout(.sizeThatFits)
+        }
+    }
+}
+
+struct TopBarView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "line.horizontal.3")
+                .font(.system(size: 25))
+                .foregroundColor(.black)
+            
+            Spacer()
+            
+            Image(systemName: "book.fill")
+                .font(.system(size: 35))
+                .foregroundColor(.black)
+            
+            Spacer()
+            
+            Image(systemName: "archivebox.fill")
+                .font(.system(size: 25))
+                .foregroundColor(.black)
+        }
+        .padding(15)
+    }
+}
+
+struct BottomBarView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "xmark")
+                .font(.system(size: 25))
+                .foregroundColor(.black)
+            
+            Spacer()
+            
+            Button(action: {
+                
+            }) {
+                Text("Comprar el curso")
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding(.all, 20)
+                    .background(Color.black)
+                    .cornerRadius(12)
+            }
+            .padding(15)
+            
+            Spacer()
+            
+            Image(systemName: "heart")
+                .font(.system(size: 25))
+                .foregroundColor(.black)
+        }
+        .padding(15)
     }
 }
